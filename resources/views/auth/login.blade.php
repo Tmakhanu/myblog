382bb00e91eb4e595c122f5fc308bf151df94496
Tmakhanu <teclamakhanu@gmail.com>
2021-02-22 11:05:50 +0300

    modified the login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,37 @@
 @extends('layouts.front.design')
 @section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <p class="text-center">Login</p>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('user.register') }}" method="POST"> 
+                @csrf
+                <div class=" form-group row">
+                    <div class="col-md-4">
+                        &nbsp;
+                </div>
+                 <div class="col-md-4">
+                    <label for="username" class="form-label">User Name</label></div>
+                <input type="text" class="form-control" name="username" placeholder="John Doe" required>
+                <div class="col-md-4">
+                    &nbsp;
+                </div>
+            </div>
+            <div class=" form-group row">
+                <div class="col-md-4">
+                    &nbsp;
+            </div>
+            <div class="col-md-4">
+                <label for="password" class="form-label">Password</label></div>
+            <input type="password" class="form-control" name="password" required>
+            <div class="col-md-4">
+                &nbsp;
+            </div>
+        </div><div class=" form-group row">
+            <div class="col-md-4">
+                &nbsp;
+        </div>
     
 @endsection
